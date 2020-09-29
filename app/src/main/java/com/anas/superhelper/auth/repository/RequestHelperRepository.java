43193283d5281cad9eaa5ea251fa060821aca2e0
Anas Mohamed <anaskamal659@gmail.com>
2020-09-29 23:28:14 +0200

    put request helper data into firebase

--- app/src/main/java/com/anas/superhelper/auth/repository/RequestHelperRepository.java
+++ app/src/main/java/com/anas/superhelper/auth/repository/RequestHelperRepository.java
@@ -1,7 +1,11 @@
 package com.anas.superhelper.auth.repository;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.anas.superhelper.auth.models.RequestHelper;
+import com.anas.superhelper.auth.models.User;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -9,9 +13,10 @@ public class RequestHelperRepository {
     private FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference mRef = database.getReference().child("Users");
-    public void insertHelperRequestData() {
-       // RequestHelper requestHelper = new RequestHelper();
+    public void insertHelperRequestData(RequestHelper requestHelper) {
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
 
-      //  mRef.child("users").child(userId).setValue(user);
+        mRef.child("users").child(firebaseUser.getUid()).child("requests").push().setValue(requestHelper);
+
     }
 }
--- app/src/main/java/com/anas/superhelper/auth/view/RequestHelperFragment.java
+++ app/src/main/java/com/anas/superhelper/auth/view/RequestHelperFragment.java
@@ -3,6 +3,7 @@ package com.anas.superhelper.auth.view;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -18,31 +19,36 @@ import android.widget.Toast;
 
 import com.anas.superhelper.R;
 import com.anas.superhelper.auth.models.RequestHelper;
+import com.anas.superhelper.auth.viewmodels.RequestHelperViewModel;
+import com.anas.superhelper.auth.viewmodels.SignUpViewModel;
 
-public class RequestHelperFragment extends Fragment implements AdapterView.OnItemSelectedListener{
-    String[] whoIsTheHelpFor = { "Individual Male", "Individual Female", "Couple"};
-    String[] whatYouNeedHelpWith = {"read book","crossing the street","old man helping"};
-    Spinner whoIsTheHelpForSpinner,whatYouNeedHelpWithSpinner;
-    EditText relevantTagsET,requestTitleET,requestDetailsET;
-    String whoIsTheHelpForText,whatYouNeedHelpWithText;
-Button getLocationBtn;
+public class RequestHelperFragment extends Fragment implements AdapterView.OnItemSelectedListener {
+    String[] whoIsTheHelpFor = {"Individual Male", "Individual Female", "Couple"};
+    String[] whatYouNeedHelpWith = {"read book", "crossing the street", "old man helping"};
+    Spinner whoIsTheHelpForSpinner, whatYouNeedHelpWithSpinner;
+    EditText relevantTagsET, requestTitleET, requestDetailsET;
+    String whoIsTheHelpForText, whatYouNeedHelpWithText;
+    Button getLocationBtn;
+    RequestHelperViewModel requestHelperViewModel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_request_helper, container, false);
-        relevantTagsET = (EditText)view.findViewById(R.id.relevant_tags_et);
-        requestTitleET = (EditText)view.findViewById(R.id.request_title_et);
-        requestDetailsET = (EditText)view.findViewById(R.id.request_details_et);
-        whoIsTheHelpForSpinner = (Spinner)view.findViewById(R.id.how_is_the_help_for_spinner);
-        whatYouNeedHelpWithSpinner = (Spinner)view.findViewById(R.id.what_you_need_help_with_sppiner);
-        getLocationBtn = (Button)view.findViewById(R.id.getLocationBtn);
+        relevantTagsET = (EditText) view.findViewById(R.id.relevant_tags_et);
+        requestTitleET = (EditText) view.findViewById(R.id.request_title_et);
+        requestDetailsET = (EditText) view.findViewById(R.id.request_details_et);
+        whoIsTheHelpForSpinner = (Spinner) view.findViewById(R.id.how_is_the_help_for_spinner);
+        whatYouNeedHelpWithSpinner = (Spinner) view.findViewById(R.id.what_you_need_help_with_sppiner);
+        getLocationBtn = (Button) view.findViewById(R.id.getLocationBtn);
         whoIsTheHelpForSpinner.setOnItemSelectedListener(this);
         whatYouNeedHelpWithSpinner.setOnItemSelectedListener(this);
+        requestHelperViewModel = ViewModelProviders.of(this).get(RequestHelperViewModel.class);
 
         //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter arrayAdapterWhoIsTheHelpFor = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_item,whoIsTheHelpFor);
+        ArrayAdapter arrayAdapterWhoIsTheHelpFor = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, whoIsTheHelpFor);
         arrayAdapterWhoIsTheHelpFor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        ArrayAdapter arrayAdapterWhatYouNeedHelpWith = new ArrayAdapter(getActivity(),android.R.layout.simple_spinner_item,whatYouNeedHelpWith);
+        ArrayAdapter arrayAdapterWhatYouNeedHelpWith = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, whatYouNeedHelpWith);
         arrayAdapterWhatYouNeedHelpWith.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         whatYouNeedHelpWithSpinner.setAdapter(arrayAdapterWhatYouNeedHelpWith);
@@ -55,13 +61,13 @@ Button getLocationBtn;
                 RequestHelper requestHelper = new RequestHelper();
                 requestHelper.setLatitude(getArguments().getString(""));
                 requestHelper.setLatitude(getArguments().getString(""));
-requestHelper.setRelevantTags(relevantTagsET.getText().toString());
-requestHelper.setRequestTitle(requestTitleET.getText().toString());
-requestHelper.setRequestDetails(requestDetailsET.getText().toString());
-requestHelper.setWhatYouNeedHelpWith(whatYouNeedHelpWithText);
-requestHelper.setWhoIsTheHelpFor(whoIsTheHelpForText);
-
-                startActivity(new Intent(getActivity(),MapsFragment.class));
+                requestHelper.setRelevantTags(relevantTagsET.getText().toString());
+                requestHelper.setRequestTitle(requestTitleET.getText().toString());
+                requestHelper.setRequestDetails(requestDetailsET.getText().toString());
+                requestHelper.setWhatYouNeedHelpWith(whatYouNeedHelpWithText);
+                requestHelper.setWhoIsTheHelpFor(whoIsTheHelpForText);
+                requestHelperViewModel.insertHelperRequestData(requestHelper);
+                startActivity(new Intent(getActivity(), MapsFragment.class));
             }
         });
         return view;
@@ -69,12 +75,10 @@ requestHelper.setWhoIsTheHelpFor(whoIsTheHelpForText);
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        if(parent.getId() == R.id.how_is_the_help_for_spinner)
-        {
+        if (parent.getId() == R.id.how_is_the_help_for_spinner) {
             whoIsTheHelpForText = whoIsTheHelpFor[position];
         }
-        if(parent.getId() == R.id.what_you_need_help_with_sppiner)
-        {
+        if (parent.getId() == R.id.what_you_need_help_with_sppiner) {
             whatYouNeedHelpWithText = whatYouNeedHelpWith[position];
         }
     }
