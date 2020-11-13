fe0651ed380497f1f1a150f2ec04716c6f298293
Anas Mohamed <anaskamal659@gmail.com>
2020-11-13 13:44:08 +0200

    change disabled type text view to be spinner

--- app/src/main/java/com/anas/superhelper/auth/view/SignUpDisabledPersonalInfoFragment.java
+++ app/src/main/java/com/anas/superhelper/auth/view/SignUpDisabledPersonalInfoFragment.java
@@ -1,17 +1,16 @@
 package com.anas.superhelper.auth.view;
 
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.Spanned;
-import android.text.TextWatcher;
-import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -20,20 +19,19 @@ import androidx.fragment.app.FragmentTransaction;
 import com.anas.superhelper.R;
 import com.anas.superhelper.androidchipbubbletext.ChipBubbleText;
 import com.anas.superhelper.auth.models.User;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipDrawable;
-import com.tylersuehr.chips.ChipsInputLayout;
 
 
-public class SignUpDisabledPersonalInfoFragment extends Fragment {
-    EditText addressEditText, jobEditText, disabledTypeEditText;
+public class SignUpDisabledPersonalInfoFragment extends Fragment implements
+        AdapterView.OnItemSelectedListener {
+    EditText addressEditText, jobEditText;
     MultiAutoCompleteTextView interestsEditText;
-
+    Spinner disabledTypeSpinner;
     Button nextBtn;
     Fragment signUpLastFragment;
     Bundle bundle;
     User user;
-    String[] values = {"coutinho","suarez","messi","ronaldo","silva","aguero"};
+    String[] values = {"coutinho", "suarez", "messi", "ronaldo", "silva", "aguero"};
+    String[] country = {"India", "USA", "China", "Japan", "Other"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,9 +39,15 @@ public class SignUpDisabledPersonalInfoFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_sign_up_disabled_personal_info, container, false);
         addressEditText = (EditText) view.findViewById(R.id.address_et);
         jobEditText = (EditText) view.findViewById(R.id.job_et);
-        disabledTypeEditText = (EditText) view.findViewById(R.id.disabled_type_et);
+        disabledTypeSpinner = (Spinner) view.findViewById(R.id.disable_type_spinner);
         interestsEditText = (MultiAutoCompleteTextView) view.findViewById(R.id.interests_text_view);
         nextBtn = (Button) view.findViewById(R.id.next_btn_disabled_personal_info);
+        disabledTypeSpinner.setOnItemSelectedListener(this);
+        //Creating the ArrayAdapter instance having the country list
+        ArrayAdapter disabledTypeSpinnerArrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_spinner_item, country);
+        disabledTypeSpinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        disabledTypeSpinner.setAdapter(disabledTypeSpinnerArrayAdapter);
         ChipBubbleText cp = new ChipBubbleText(getActivity(), interestsEditText, values, 1);
         cp.setChipColor("#9999FF");
         cp.setChipTextSize(20);
@@ -55,29 +59,37 @@ public class SignUpDisabledPersonalInfoFragment extends Fragment {
         nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("chips",interestsEditText.getText().toString());
+                Log.i("chips", interestsEditText.getText().toString());
 
                 if (addressEditText.getText().toString().isEmpty()) {
                     addressEditText.setError("Field cannot be empty");
                 } else if (jobEditText.getText().toString().isEmpty()) {
                     jobEditText.setError("Field cannot be empty");
-                } else if (disabledTypeEditText.getText().toString().isEmpty()) {
-                    disabledTypeEditText.setError("Field cannot be empty");
-                }else{
+                } else {
                     FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
                     FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
                     fragmentTransaction.replace(R.id.frameLayout, signUpLastFragment);
                     fragmentTransaction.addToBackStack(null);
                     user.setJob(jobEditText.getText().toString());
                     user.setAddress(addressEditText.getText().toString());
-                    user.setDisabledType(disabledTypeEditText.getText().toString());
-                    user.setInterests(interestsEditText.getText().toString().isEmpty() ? "" :interestsEditText.getText().toString());
-                    bundle.putParcelable("user",user);
+                    user.setInterests(interestsEditText.getText().toString().isEmpty() ? "" : interestsEditText.getText().toString());
+                    bundle.putParcelable("user", user);
                     signUpLastFragment.setArguments(bundle);
                     fragmentTransaction.commit();
                 }
             }
         });
         return view;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        user.setDisabledType(country[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        user.setDisabledType(country[0]);
+
     }
 }
